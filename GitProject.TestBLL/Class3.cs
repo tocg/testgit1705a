79005b8f1703d9ee863220f8995e50cdc63d8121
tocg <max.liang@lyetech.com>
2019-12-02 13:32:17 +0800

    1202-1332

---
+++ GitProject.TestBLL/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProject.TestBLL
+{
+    class Class3
+    {
+        public void Add()
+        {
+            //
+        }
+    }
+}
